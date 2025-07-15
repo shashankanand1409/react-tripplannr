1d26dae6f1edb35c1ccb01a0427fef74dd7050b0
shashankanand1409 <shashankanand1409@gmail.com>
2025-07-15 12:05:14 +0530

    add bg-image

--- vite-react/src/App.jsx
+++ vite-react/src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
 import './App.css'
 import Navbar from './Navbar'
+import BG from './assets/homepage-bg.jpg'
 
 function App() {
 
   return (
-    
       <div>
+        
         <Navbar/>
+        <img className='bg' src={BG} alt="Background Image"/>
       </div>
   )
 }
