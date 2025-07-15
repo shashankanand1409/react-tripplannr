fcd26885f131cdd619300c48dd08a9da444d85ca
shashankanand1409 <shashankanand1409@gmail.com>
2025-07-15 11:46:07 +0530

    Initial commit

---
+++ git-initialize.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+git init
+git add .
+git commit -m "Initial commit"
+
+echo "✅ Git repository initialized and first commit made!"
---
+++ vite-react/src/Navbar.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import './navbar.css'
+
+const Navbar = () => {
+  return (
+    <>
+    <header>
+    <div className="navbar">
+        <p><span className='span'>TRIP</span> PLANNR</p>
+        <p>Home</p>
+        <p>Carpool</p>
+        <p>Tickets</p>
+        <p>Hotels</p>
+        <p>Restaurants</p>
+        <p>Live Events</p>
+        <p>Contact Us</p>
+
+    <div class="icons">
+        <i class="fas fa-search" id="search-btn"></i>
+        <i class="fas fa-user" id ="login-btn"></i>
+    </div>
+    </div>
+    </header>
+
+    </>
+  )
+}
+
+export default Navbar
